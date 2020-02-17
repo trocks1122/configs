fb618ca8687f3441bca0095581a20b14e27ba7aa
Trevor <trocks1122@gmail.com>
2020-02-16 21:29:21 -0500

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
-ln -sv ~/configs/tmux.conf ~/.tmux.conf
-sudo ln -sv ~/configs/vimrc.local /etc/vim/vimrc.local
+
+if [[ ! -f /etc/vim/vimrc.local ]]; then
+	sudo ln -sv $PWD/vimrc.local /etc/vim/vimrc.local
+fi
+
+#from .tmux/README
+if [[ ! -f ~/.tmux.conf ]]; then
+	ln -sv $PWD/.tmux/.tmux.conf ~/.tmux.conf 
+fi
+if [[ ! -f ~/.tmux.conf.local ]]; then
+	cp $PWD/.tmux.conf.local ~/.tmux.conf.local
+fi
+
+echo "export EDITOR=vim" >> ~/.bashrc
+echo "export TERM=xterm-256color"  >> ~/.bashrc
+
 
 echo "Made symbolic links for vimrc and tmux.conf"
